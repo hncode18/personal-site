ada47f0f1d9769624997a268ee105cf61afbe1c5
Hoang Nguyen <nguyeho2@oregonstate.edu>
2020-06-16 22:07:17 -0700

    Add name and links to Github and LinkedIn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hoang Nguyen</h1>
+        <div>
+            <a href="https://github.com/hncode18">
+                <i class="fa fa-github-square" aria-hidden="true"></i>
+            </a>
+        </div>
+
+        <div>
+            <a href="https://www.linkedin.com/in/hoang-nguyen21/">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+        </div>
     </div>
   );
 }
