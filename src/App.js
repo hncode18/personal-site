fd8949f1aab9aa0207a5cebb61e406ad4c4d2a9c
Hoang Nguyen <nguyeho2@oregonstate.edu>
2020-06-19 19:25:08 -0700

    Added education info and rearange elements

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ function App() {
     <div className="App">
         <div id="name-div">
             <i class="fa fa-code fa-5x" id="logo" aria-hidden="true"></i>
-            <h1 id="name">Hoang Nguyen</h1>
+            <h1 id="name">{"{ Hoang Nguyen }"}</h1>
             <h3 id="title">Full Stack Developer</h3>
         </div>
         
-        <div className="link-div">
-            <a className="bio-link" href="https://github.com/hncode18">
-                <i className="fa fa-github-square fa-2x" aria-hidden="true"></i>
-            </a>
-            <a className="bio-link" href="https://www.linkedin.com/in/hoang-nguyen21/">
-                <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-            </a>
-        </div>
+        <div id="bio-container">
+            <div class="education-div">
+                <p id="school">Oregon State University</p>
+                <p>B.S in Computer Science</p>
+            </div>
+
+            <div className="link-container">
+                <div className="link-div" id="github">
+                    <i className="fa fa-github-square fa-2x" aria-hidden="true"></i>
+                    <a className="bio-link" href="https://github.com/hncode18">hncode18</a>
+                </div>
+                <div className="link-div" id="linkedin">
+                    <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                    <a className="bio-link" href="https://www.linkedin.com/in/hoang-nguyen21/">hoang-nguyen21</a>
+                </div>
+            </div>
+        </div>        
     </div>
   );
 }
