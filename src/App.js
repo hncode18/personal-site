9eb16e4a3b6346de9ce6d4b9f2094e1af228e1bc
Hoang Nguyen <nguyeho2@oregonstate.edu>
2020-06-23 23:48:05 -0700

    Change font, and add typing animation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
 import React from 'react';
 import './App.css';
 
+class Title extends React.Component 
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            outputText: "",
+        }
+
+        this.text = "Full Stack Developer";
+        this.i = 0;
+    }
+
+    componentDidMount()
+    {
+        this.timerId = setInterval(() => this.animate(), 60);
+    }
+
+    componentWillUnmount()
+    {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+        }
+    }
+
+    animate()
+    {
+        if (this.i < this.text.length) {
+            this.setState((state) => ({
+                outputText: state.outputText + this.text.charAt(this.i)
+            }));
+            this.i += 1;
+        }
+        else {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    render()
+    {
+        return <h3 id={this.props.id}>{this.state.outputText}</h3>
+    }
+}
+
 function App() {
   return (
     <div className="App">
         <div id="name-div">
             <i class="fa fa-code fa-5x" id="logo" aria-hidden="true"></i>
-            <h1 id="name">{"{ Hoang Nguyen }"}</h1>
-            <h3 id="title">Full Stack Developer</h3>
+            <h1 id="name">{"Hoang Nguyen"}</h1>
+            {/* <h3 id="title">Full Stack Developer</h3> */}
+            <Title id="title" />
         </div>
         
         <div id="bio-container">
             <div class="education-div">
                 <p id="school">Oregon State University</p>
-                <p>B.S in Computer Science</p>
+                <p id="degree">B.S. Computer Science</p>
             </div>
 
             <div className="link-container">
-                <div className="link-div" id="github">
-                    <i className="fa fa-github-square fa-2x" aria-hidden="true"></i>
-                    <a className="bio-link" href="https://github.com/hncode18">hncode18</a>
-                </div>
-                <div className="link-div" id="linkedin">
-                    <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-                    <a className="bio-link" href="https://www.linkedin.com/in/hoang-nguyen21/">hoang-nguyen21</a>
+                <div className="link-icon-div">
+                    <div className="icon-div">
+                        <i className="fa fa-github-square fa-2x" aria-hidden="true"></i>
+                        <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                    </div>
+                    <div className="link-div">
+                        <a className="bio-link" href="https://github.com/hncode18">hncode18</a>                    
+                        <a className="bio-link" href="https://www.linkedin.com/in/hoang-nguyen21/">hoang-nguyen21</a>
+                    </div>
                 </div>
             </div>
         </div>        
