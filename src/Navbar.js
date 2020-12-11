0674098960d5e1388d4491a7eb130ceba4f3cb77
Hoang Nguyen <nguyeho2@oregonstate.edu>
2020-12-10 16:17:06 -0800

    Add resume link to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import resume from './Resume.pdf';
 
 function Navbar(props) {
     return (
@@ -10,6 +11,7 @@ function Navbar(props) {
             </div>
             <div id="right-nav">
                 <Link to="/project">Projects</Link>
+                <a href={resume}>Resume</a>
             </div>
         </div>
     );
