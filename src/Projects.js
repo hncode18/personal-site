74a3b0d1c89b9f5b90d3d0ef93ccb1ce1ae3ccdf
Hoang Nguyen <nguyeho2@oregonstate.edu>
2021-04-29 19:00:12 -0700

    Add more projects and update resume

--- src/Projects.js
+++ src/Projects.js
@@ -38,9 +38,12 @@ function Projects()
             <div className="page-title-div">
                 <h1 id="page-title">Projects</h1>
             </div>
-            <ProjectCard title="OSU CS Applied Plan Portal" tools="React, Emotion.sh, Node.JS, MySQL"
+            <ProjectCard title="OSU CS Applied Plan Portal" tools="React, express.js, Node.JS, MySQL, AWS"
                 desc="/project/osu"
                 code="https://github.com/OSU-Applied-Plan-Portal-2020-2021/cs-applied-plan-portal"/>
+
+            <ProjectCard title="RSS Reader" tools="Handlebars, express.js, Node.JS, MongoDB"
+                code="https://github.com/OSU-CS290-Sp18/final-project-group-5"/>
         </div>
     );
 }
